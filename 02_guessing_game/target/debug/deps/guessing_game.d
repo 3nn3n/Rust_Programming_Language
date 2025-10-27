5042776e65100a14131260d5e0461014031155b0
Nitesh Negi <nitesh_negi@outlook.com>
2025-10-27 19:50:18 +0530

    Lil change

--- 02_guessing_game/target/debug/deps/guessing_game.d
+++ 02_guessing_game/target/debug/deps/guessing_game.d
@@ -1,5 +1,5 @@
-D:\Rust_PL\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+D:\Rust_PL\02_guessing_game\target\debug\deps\guessing_game.d: src\main.rs
 
-D:\Rust_PL\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+D:\Rust_PL\02_guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
 
 src\main.rs:
--- 02_guessing_game/target/debug/guessing_game.d
+++ 02_guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-D:\Rust_PL\guessing_game\target\debug\guessing_game.exe: D:\Rust_PL\guessing_game\src\main.rs
+D:\Rust_PL\02_guessing_game\target\debug\guessing_game.exe: D:\Rust_PL\02_guessing_game\src\main.rs

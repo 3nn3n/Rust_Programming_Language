3e6c93067fa4cacadb30a3e061c611c02a15ea31
Nitesh Negi <nitesh_negi@outlook.com>
2025-10-28 09:39:18 +0530

    Add Datatypes

---
+++ variables/target/debug/deps/variables-d7216a90429ed25b.d
@@ -0,0 +1,5 @@
+D:\Rust_PL\variables\target\debug\deps\variables-d7216a90429ed25b.d: src\main.rs
+
+D:\Rust_PL\variables\target\debug\deps\libvariables-d7216a90429ed25b.rmeta: src\main.rs
+
+src\main.rs:
